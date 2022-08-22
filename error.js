0cf671eee1bc96ca2c8f464f37f90375c3527c4c
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-22 23:29:40 +0600

    teaching error

--- error.js
+++ error.js
@@ -1,6 +1,13 @@
 //Range error
 
 const number = [23, 23, 54, 34];
-console.log(number);
+// console.log(number);
 number.length = 2;
-console.log(number.length);
+// console.log(number.length);
+//Reference error
+// console.log(money)
+const money = 40;
+let i = 'hello';
+for(i = 0; i<5; i++){
+    console.log(i);
+}
