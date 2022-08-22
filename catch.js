cbe2f22ab49af23c836147dc84ed4b7acaacb32d
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-23 00:17:47 +0600

    try catch finally error handeling

---
+++ catch.js
@@ -0,0 +1,26 @@
+const day = 'day the din'
+console.log(day)
+console.log(25)
+
+// let distric = 'bbaria'
+// console.log(distric);
+console.log(61);
+console.log(62);
+console.log(63);
+
+try{
+    console.log('inside try')
+    // console.log(student)
+    
+    console.log(distric);
+    let distric = 'bbaria';
+    console.log('After error');
+}
+catch(error){
+    console.log('Hello World catch');
+    // console.log(error);
+}
+finally{
+    console.log('Finally done');
+}
+console.log('Last line of the code');
