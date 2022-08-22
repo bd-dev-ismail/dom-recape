8f46f3ed983f1d9bd3d146d7243d2ae10041f5c3
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-23 00:51:40 +0600

    understading isNaN

---
+++ validation.js
@@ -0,0 +1,10 @@
+const number = 3;
+if(typeof number == 'number'){
+    console.log('it is a number');
+}
+else{
+    console.log('It is not a number');
+}
+
+const numbers = [12,23 ,23 ,22, 23];
+console.log(Array.isArray(numbers) );
